e21b9cc01413daffd71c9a1209cedd0cf1802064
Danil Shepelev <shdaig@gmail.com>
2024-05-02 23:52:20 +0300

    adaptive joystick

--- mobile_control.gd
+++ mobile_control.gd
@@ -2,43 +2,38 @@ extends CanvasLayer
 
 signal use_move_vector
 
-var move_vector = Vector2.ZERO
-var texture_center
-var object_scale
-var button_radius
-var joystick_active = false
+var outer_circle_radius
+
 
 func _input(event):
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if $TouchScreenButton.is_pressed():
-			var move_vector = calculate_move_vector(event.position)
-			var texture_center = $TouchScreenButton.position + Vector2(62.5, 62.5)
-			print(move_vector)
-			emit_signal("use_move_vector", move_vector)
-			joystick_active = true
-			$Inner_circle.position = move_vector * 62.5 + texture_center
-			$Inner_circle.visible = true
-		else:
-			emit_signal("use_move_vector", Vector2.ZERO)
+		if event.is_pressed():
+			$OuterCircle.visible = true
+			$OuterCircle.position = event.position
+			$InnerCircle.visible = true
+		var move_vector = calculate_move_vector(event.position)
+		emit_signal("use_move_vector", move_vector)
+		$InnerCircle.position = move_vector * outer_circle_radius + $OuterCircle.position
 			
 	if event is InputEventScreenTouch:
-		if event.pressed == false:
-			joystick_active = false
-			$Inner_circle.visible = false
+		if not event.is_pressed():
+			emit_signal("use_move_vector", Vector2.ZERO)
+			$OuterCircle.visible = false
+			$InnerCircle.visible = false
+			
 			
 func calculate_move_vector(event_position):
-	var normalized_event_position = (event_position - texture_center) / 62.5
-	
+	var normalized_event_position = (event_position - $OuterCircle.position) / outer_circle_radius
 	if normalized_event_position.length() > 1:
 		normalized_event_position = normalized_event_position.normalized() 
-	
 	return normalized_event_position
 
+
 func _ready():
-	button_radius = $TouchScreenButton.shape.radius
-	object_scale = $TouchScreenButton.scale
-	texture_center = $TouchScreenButton.position + object_scale * button_radius
-	$Inner_circle.visible = false
+	outer_circle_radius = $OuterCircle.get_rect().size[0] / 2 * $OuterCircle.scale
+	$InnerCircle.visible = false
+	$OuterCircle.visible = false
+	
 
 func _process(delta):
 	pass
